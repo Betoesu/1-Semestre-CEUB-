aadcc841d27674de2223b6d7d92c296302ddac87
Pedro Sarmento <pedrosarmento1412@gmail.com>
2025-03-18 22:03:11 -0300

    Trabalho 1

---
+++ Trabalho2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main (void)
+{
+    float fixo, vendas, salario;
+    char nome [25];
+
+    scanf("%s", &nome);
+    scanf("%f", &fixo);
+    scanf("%f", &vendas);
+
+    printf("TOTAL = R$%.2f", salario = 0.15 * vendas + fixo);
+}
+//Pedro Carolino Barreto Sarmento RA: 22501994
+ 
